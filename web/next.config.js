a7726982e296f2ede0a5ef9ef00003e573bfca8a
jakops88-hub <jakops88@gmail.com>
2025-09-10 11:57:01 +0200

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -2,19 +2,12 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' }
-    ]
-  }
-};
-// web/next.config.js
-module.exports = {
-  images: {
-    remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'addrevenue.io' },
       { protocol: 'https', hostname: '*.addrevenue.io' },
-      { protocol: 'https', hostname: 'api.adrevenue.com' }, // om bilder nånsin kommer härifrån
+      { protocol: 'https', hostname: 'api.adrevenue.com' },
     ],
   },
 };
+
+module.exports = nextConfig;
